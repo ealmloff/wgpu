0589934c58743e1d35d601ae163a71da2aeea808
Evan Almloff <evanalmloff@gmail.com>
2025-12-28 08:14:29 -0600

    implement subgroup detection

--- wgpu/src/backend/webgpu.rs
+++ wgpu/src/backend/webgpu.rs
@@ -711,7 +711,7 @@ fn map_map_mode(mode: crate::MapMode) -> u32 {
     }
 }
 
-const FEATURES_MAPPING: [(wgt::Features, webgpu_sys::GpuFeatureName); 16] = [
+const FEATURES_MAPPING: [(wgt::Features, webgpu_sys::GpuFeatureName); 17] = [
     (
         wgt::Features::DEPTH_CLIP_CONTROL,
         webgpu_sys::GpuFeatureName::DepthClipControl,
@@ -775,6 +775,10 @@ const FEATURES_MAPPING: [(wgt::Features, webgpu_sys::GpuFeatureName); 16] = [
     (
         wgt::Features::CLIP_DISTANCES,
         webgpu_sys::GpuFeatureName::ClipDistances,
+    ),
+    (
+        wgt::Features::SUBGROUP,
+        webgpu_sys::GpuFeatureName::Subgroups,
     ),
 ];
 
